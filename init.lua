c73036c9eaf557fb20127295f4f9f967834ad67b
Kiril Tonev <tonev@fastmail.com>
2024-03-02 13:48:31 +0100

    Remove stray option.

--- init.lua
+++ init.lua
@@ -159,7 +159,6 @@ require('lazy').setup {
     on_attach = function(bufnr)
       local gs = require 'gitsigns'
 
-      vim.o.background = 'dark'
       vim.keymap.set('n', '<leader>gp', gs.prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
       vim.keymap.set('n', '<leader>gn', gs.next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
       vim.keymap.set('n', '<leader>ph', gs.preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
