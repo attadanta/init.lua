bc022f3eb919692f4f5ea268f3f380b4a1e5c7eb
Kiril Tonev <tonev@fastmail.com>
2024-03-04 19:01:58 +0100

    Get copilot.

--- init.lua
+++ init.lua
@@ -122,6 +122,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup {
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'github/copilot.vim',
 
   -- Git related plugins
   'tpope/vim-fugitive',
